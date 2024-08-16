24bed9b88af3db882b3b3148d629094ce34bd9c5
likeeyong17@gmail.com <likeeyong17@gmail.com>
2024-08-16 05:13:02 +0000

    Created test_bank_frm, test_bank_frm.ItemTemplate1 and edit_question_frm; Edited settings and main_frm

---
+++ client_code/edit_question_frm/__init__.py
@@ -0,0 +1,13 @@
+from ._anvil_designer import edit_question_frmTemplate
+from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class edit_question_frm(edit_question_frmTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
--- client_code/main_frm/__init__.py
+++ client_code/main_frm/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import main_frmTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 
 class main_frm(main_frmTemplate):
@@ -8,3 +11,15 @@ class main_frm(main_frmTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def main_options_clicked(self, **event_args):
+    """This method is called when this radio button is selected"""
+    if self.build_quiz_rdb.checked:
+      # create a quiz
+      pass
+    elif self.take_quiz_rdb.checked:
+      # take a quiz
+      pass
+    else:
+      # go to my test bank
+      pass
