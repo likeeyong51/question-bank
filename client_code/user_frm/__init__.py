8193490033e1e4147b00daebd92d9cb911d3cd98
likeeyong17@gmail.com <likeeyong17@gmail.com>
2024-09-11 03:24:14 +0000

    added user signup and login

--- client_code/user_frm/__init__.py
+++ client_code/user_frm/__init__.py
@@ -11,7 +11,13 @@ class user_frm(user_frmTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+    # Any code you write here will run before the form opens
+    # populate group dropdown items
+    self.group = [
+      grp['group'] for grp in app_tables.group_tbl.search()
+    ]
+    # print(self.item['group'])
+    self.group_drp.items = self.group
 
   def sign_up_chk_change(self, **event_args):
     """This method is called when this checkbox is checked or unchecked"""
@@ -29,3 +35,73 @@ class user_frm(user_frmTemplate):
       self.firstname_txb.visible = False
       self.lastname_txb.visible  = False
       self.email_txb.visible     = False
+
+  def login_signup_btn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    if self.sign_up_chk.checked:
+      # sign up mode
+      if self.validate_input_data():
+        # print(self.item)
+        if not anvil.server.call('create_user', self.item):
+          Notification('User already exists.').show()
+          self.reset_form()
+          return
+
+        # check if new user has been successfully added into the user table 
+        if anvil.server.call('authenticate_user', self.item['username'], self.item['password']):
+          self.reset_form()
+          Notification('You are signed up! Please attempt your first login', 
+                       title='Welcome to your Test Bank App').show()
+      else:
+        alert('Please enter your username, password and email', title='Error')
+    else:
+      # log in mode
+      if self.validate_input_data():
+        user_exist = anvil.server.call('authenticate_user', self.item['username'], self.item['password'])
+    
+        if user_exist:
+          # alert("Welcome to your reminder app")
+          open_form('main_frm', username=self.item['username'])
+        else:
+          alert('Please check your login credentials and try again...', title='Error')
+
+  def validate_input_data(self):
+    # validate login data
+    if self.username_txb.text == '':
+      alert('Please enter your username',    title='Error')
+      return False
+    if self.password_txb.text == '':
+      alert('Please enter a valid password', title='Error')
+      return False
+    if self.group_drp.selected_value not in self.group_drp.items:
+      alert('Please select your user group', title='Error')
+      return False
+      
+    if self.sign_up_chk.checked:
+      # sign up mode - additional validations
+      if self.firstname_txb.text == '':
+        alert('Please enter your first name', title='Error')
+        return False
+      if self.lastname_txb.text == '':
+        alert('Please enter your last name',  title='Error')
+        return False
+      if self.email_txb.text == '':
+        alert('Please enter your email',      title='Error')
+        return False
+
+    return True # no error'
+
+  def reset_form(self):
+    """reset form to login mode"""
+    self.sign_up_chk.checked      = False
+    self.login_signup_btn.text    = 'Log In'
+    
+    self.username_txb.text        = ''
+    self.password_txb.text        = ''
+    self.group_drp.selected_value = None
+    self.firstname_txb.text       = ''
+    self.lastname_txb.text       = ''
+    self.email_txb.text           = ''
+
+    self.username_txb.focus()
+        
---
+++ server_code/authenciation_svr_mod.py
@@ -0,0 +1,51 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+
+@anvil.server.callable
+def create_user(user_info):
+  row = app_tables.user_tbl.search(username=user_info['username'], password=user_info['password'])
+
+  # check if user exists
+  if len(row) == 1:
+    return False # user exists 
+
+  # add new user to the user table
+  # print(user_info)
+  group = app_tables.group_tbl.get(group=user_info['group'])
+  
+  app_tables.user_tbl.add_row( #**user_info)
+    username   = user_info['username'], 
+    password   = user_info['password'],
+    group      = group,
+    first_name = user_info['first_name'], 
+    last_name  = user_info['last_name'], 
+    email      = user_info['email'])
+  
+  return True # new user added to the database
+
+@anvil.server.callable
+def authenticate_user(username, password):
+  # print(username, password)
+  row = app_tables.user_tbl.search(username=username, password=password)
+
+  # if user exist
+  if len(row) == 1:
+    return True # found
+
+  # user does not exist yet
+  return False # not found
