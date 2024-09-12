0e47ae990d745dc5eda93a6856235dd0258cd2ec
likeeyong17@gmail.com <likeeyong17@gmail.com>
2024-09-12 01:10:17 +0000

    Edited main_frm

--- client_code/main_frm/__init__.py
+++ client_code/main_frm/__init__.py
@@ -13,40 +13,40 @@ class main_frm(main_frmTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-    self.item['group'] = properties['group']
+    self.item['username'] = properties['username']
+    self.item['group']    = properties['group']
 
     # process and set group privilege options
     self.set_group_privilege()
-      
 
     # populate user dropdown
     self.user_drp.items = [
-      properties['username'],
+      self.item['username'],
       'Switch user',
       'Log out'
     ]
 
   def main_options_clicked(self, **event_args):
     """This method is called when this radio button is selected"""
-    if self.open_testbank_rdb.selected:
+    if self.goto_testbank_rdb.selected:
       # go to my test bank
-      open_form('test_bank_frm')
+      open_form('test_bank_frm', group=self.item['group'])
       
     elif self.build_quiz_rdb.selected:
       # build a quiz
-      open_form('build_quiz_frm')
+      open_form('build_quiz_frm', group=self.item['group'])
       
     else:
       # take a quiz
-      open_form('quiz_frm')
+      open_form('quiz_frm', group=self.item['group'])
 
   def set_group_privilege(self):
     # hide options not available to students
     if self.item['group'] == 'Student':
-      self.open_testbank_rdb.visible = False
+      self.goto_testbank_rdb.visible = False
       self.build_quiz_rdb.visible    = False
     else:
-      self.open_testbank_rdb.visible = True
+      self.goto_testbank_rdb.visible = True
       self.build_quiz_rdb.visible    = True
 
   def user_drp_change(self, **event_args):
@@ -56,4 +56,18 @@ class main_frm(main_frmTemplate):
       open_form('user_frm')
 
     elif self.user_drp.selected_value == 'Switch user':
-      alert(content=user_frm(), large=True)
+      previous_user = self.item['username']
+      
+      switch_user = alert(
+        content=user_frm(), 
+        large=True,
+        buttons=[('Confirm Switch', True), ('Cancel', False)]
+      )
+
+      # if switch_user:
+      #   if previous_user != self.item['username']:
+      #     Notification('Switch user successful').show()
+      # else:
+      #   if previous_user == self.item['username']:
+      #     Notification('Switch user cancelled').show()
+        
